3aa59dfd172e0d43080d71da12513fca15e274e8
Menna <mennav@protonmail.com>
2024-01-29 10:04:02 -0300

    Usage of flags/params instead of user input

--- JSScanner.py
+++ JSScanner.py
@@ -1,86 +1,59 @@
-#import faster_than_requests as requests
 import concurrent.futures
 import requests
 from concurrent import futures
-#import requests as r 
-import re 
-import urllib3 
+import re
+import urllib3
 import os
 import colored
 from colored import stylize
-urllib3.disable_warnings()
-print(colored.fg("red"), 
-     "╔════════════════════════════════════════════════════════════════╗\n"
-      "║                    Devlope By 0x240x23elu                      ║\n"
-      "║                                                                ║\n"
-      "╚════════════════════════════════════════════════════════════════╝")
-print("╔════════════════════════════════════════════════════════════════╗\n"
-      "║                                                                ║\n"
-      "║                           WARNING                              ║\n"
-      "║                                                                ║\n"
-      "║      I highly recommend using this tool by using Kali Linux OS ║\n"
-      "║                                                                ║\n"
-      "║      By using this tool it means you agree with terms,         ║\n"
-      "║      conditions, and risks                                     ║\n"
-      "║                                                                ║\n"
-      "║      By using this tool you agree that                         ║\n"
-      "║      1. use for legitimate security testing                    ║\n"
-      "║      2. not for crime                                          ║\n"
-      "║      3. the use of this tool solely for                        ║\n"
-      "║         educational reasons only                               ║\n"
-      "║                                                                ║\n"
-      "║      By using this tool you agree that                         ║\n"
-      "║      1. You are willing to be charged with criminal or state   ║\n"
-      "║         law applicable by law enforcement officers             ║\n"
-      "║         and government when abused                             ║\n"
-      "║      2. the risk is borne by yourself                          ║\n"
-      "║                                                                ║\n"
-      "║         Thank you and happy pentest                            ║\n"
-      "║                                                                ║\n"
-      "╚════════════════════════════════════════════════════════════════╝")
+import argparse  # Importing the argparse library
 
-path = input("Please Enter Any File: ") 
-reg = input("Path Of Regex/Patten File: ")
-list=[] 
+urllib3.disable_warnings()
+
+# Setup argparse for command line arguments
+parser = argparse.ArgumentParser(description="Tool for finding secrets in JS files using regex.")
+parser.add_argument('-f', '--file', help="Path to the file containing URLs to scan", required=True)
+parser.add_argument('-r', '--regex', help="Path to the regex/pattern file", required=True)
+parser.add_argument('-o', '--output', help="Output file to save results", required=True)
+
+args = parser.parse_args()
+
+path = args.file
+reg = args.regex
+output_file = args.output
+
+list = []
 file1 = open(path, 'r')
-Lines = file1.readlines() 
+Lines = file1.readlines()
 count = 0
-# Strips the newline character
-for line in Lines: 
+
+for line in Lines:
     ip = line.strip()
-    print(colored.fg("white"), ip)
+    print(stylize(ip, colored.fg("white")))
     try:
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
             futures = [
                 executor.submit(
-                    lambda: requests.get(ip))
-            for _ in range(1)
-        ]
-            
+                    lambda: requests.get(ip, verify=False))
+                for _ in range(1)
+            ]
 
-        results = [
-            f.result().text
-            for f in futures
-        ]
+            results = [
+                f.result().text
+                for f in futures
+            ]
 
-        
-        
-        file2 = open(reg, 'r')
-        Lines2 = file2.readlines()
-        for line2 in Lines2: 
-            regex = line2.strip()
-        #print(regex)
-            matches = re.finditer(regex, str(results), re.MULTILINE)
-            for matchNum, match in enumerate(matches, start=1):
-    
-                print (colored.fg("green") ,"Regex: ",regex)
-                print(colored.fg("red") , "Match {matchNum} was found at: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()), '\n')
-                f = open('out.txt.txt', 'a')
-                L = [ip, '\n', "Regex: ", regex, '\n', "Match {matchNum} was found at : {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()),'\n']
-                f.writelines(L)
-                f.close()
-           
+            file2 = open(reg, 'r')
+            Lines2 = file2.readlines()
+            for line2 in Lines2:
+                regex = line2.strip()
+                matches = re.finditer(regex, str(results), re.MULTILINE)
+                for matchNum, match in enumerate(matches, start=1):
+                    print(stylize("Regex: " + regex, colored.fg("green")))
+                    print(stylize("Match {matchNum} was found at: {match}".format(matchNum=matchNum, start=match.start(), end=match.end(), match=match.group()), colored.fg("red")))
+                    with open(output_file, 'a') as f:
+                        L = [ip, '\n', "Regex: ", regex, '\n', "Match {matchNum} was found at : {match}".format(matchNum=matchNum, start=match.start(), end=match.end(), match=match.group()), '\n']
+                        f.writelines(L)
+
     except requests.exceptions.RequestException as e:
-        # A serious problem happened, like an SSLError or InvalidURL
-        print("Error: {}".format(e))    
-
+        print("Error: {}".format(e))
